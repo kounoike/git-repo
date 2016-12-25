dccbb7ff9b8cc70e869a054b12c27b2b756f1ea4
Guyzmo <guyzmo+github@m0g.net>
2016-12-25 19:49:54 +0100

    🚧 BB gists and requests support ; 📼 Added integration tests and cassettes
    Signed-off-by: Guyzmo <guyzmo+github@m0g.net>

--- tests/integration/test_github.py
+++ tests/integration/test_github.py
@@ -307,37 +307,37 @@ class Test_Github(GitRepoTestCase):
     def test_32_request_create(self):
         r = self.action_request_create(namespace=self.namespace,
                 repository='test_create_requests',
-                branch='pr-test',
+                source_branch='pr-test',
+                target_branch='master',
                 title='PR test',
-                description='PR description',
-                service='github')
-        assert r == {'local': 'pr-test', 'ref': 1, 'remote': 'PR test'}
+                description='PR description')
+        assert r == {'local': 'pr-test', 'ref': 1, 'remote': 'master'}
 
     def test_32_request_create__bad_branch(self):
         with pytest.raises(ResourceError):
             self.action_request_create(namespace=self.namespace,
                     repository='test_create_requests',
-                    branch='does_not_exists',
+                    source_branch='does_not_exists',
+                    target_branch='master',
                     title='PR test',
-                    description='PR description',
-                    service='github')
+                    description='PR description')
 
     def test_32_request_create__bad_repo(self):
         with pytest.raises(ResourceNotFoundError):
             r = self.action_request_create(namespace=self.namespace,
                     repository='does_not_exists',
-                    branch='pr-test',
+                    source_branch='pr-test',
+                    target_branch='master',
                     title='PR test',
-                    description='PR description',
-                    service='github')
+                    description='PR description')
 
     def test_32_request_create__guess_branch(self):
         r = self.action_request_create(namespace=self.namespace,
                 repository='test_create_requests',
-                branch=None,
+                source_branch=None,
+                target_branch=None,
                 title='PR test',
-                description='PR description',
-                service='github')
+                description='PR description')
 
     def test_33_open(self):
         self.action_open(namespace='guyzmo',
